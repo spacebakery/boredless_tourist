8cb301a2b68d6cdcceb0c80b5dcbc8a873bdc9b2
Space Cookie <mattisiao@gmail.com>
2025-04-16 19:13:25 -0500

    added logic to find traveler destinations and convert to internal data

--- script.py
+++ script.py
@@ -12,3 +12,26 @@ destinations = ['Paris, France',
 # test traveler data
 test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
 
+
+## Part 2: Travelling To Faraway Lands
+
+# func: get destination
+def get_destination_index(destination):
+  destination_index = destinations.index(destination)
+  return destination_index
+
+# test function
+print(get_destination_index('Los Angeles, USA'))
+print(get_destination_index('Paris, France'))
+# print(get_destination_index('Hyderabad, India')) ValueError, value does not exist.
+
+# func: get traveler location
+def get_traveler_location(traveler):
+  traveler_destination = traveler[1]
+  traveler_destination_index = get_destination_index(traveler_destination)
+  return traveler_destination_index
+
+# test function
+test_destination_index = get_traveler_location(test_traveler)
+print(test_destination_index)
+
