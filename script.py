c8a24923ed4335d2bf82501bdd347d2fa31ebe1d
Space Cookie <mattisiao@gmail.com>
2025-04-16 19:11:38 -0500

    added test objects

--- script.py
+++ script.py
@@ -1,1 +1,14 @@
 ## PROJECT: The Boredless Tourist
+
+## Part 1: setup the project on Git
+
+# destinations data
+destinations = ['Paris, France', 
+                'Shanghai, China', 
+                'Los Angeles, USA', 
+                'Sao Paulo, Brazil', 
+                'Cairo, Egypt']
+
+# test traveler data
+test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
+
